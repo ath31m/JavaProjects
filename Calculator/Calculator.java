710c8ebe076771ae0febdddfb6394a4fdb8f8b22
Min Theim Kyaw <mintheimkyaw18@gmail.com>
2022-05-31 01:50:17 +0630

    input first number & second number

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
 public class Calculator 
-{
+{   
+    static int num1;
+    static int num2;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args)
-    {
-        System.out.println("--- Calculator---");
-    }
+        {
+            System.out.println("--- Calculator---");
+
+            System.out.print("Plz, Enter first number: ");
+            num1 = scanner.nextInt();
+
+            scanner.nextLine();
+
+            System.out.print("Plz, Enter first number: ");
+            num2 = scanner.nextInt();
+
+            System.out.println(num1+num2);
+        }
 }
