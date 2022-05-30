aefba210d74ac1470715567a24ce7a249adf1f7a
Min Theim Kyaw <mintheimkyaw18@gmail.com>
2022-05-31 04:07:13 +0630

    testing number range

--- Calculator/test.java
+++ Calculator/test.java
@@ -5,7 +5,6 @@ public class test
     static int num1;
     static int num2;
     static char action;
-    static String decision;
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args)
@@ -20,20 +19,28 @@ public class test
             System.out.print("Plz, Enter second number: ");
             num2 = scan.nextInt(); 
 
-            System.out.println("+\t-\t*\t/");
-            action = scan.next().charAt(0);
-
-            switch (action)
+            if( num1 < -200 || num2 < -200 || num1>200 || num2>200)
             {
-                case '+': System.out.println(num1+num2); break;
-                case '-': System.out.println("minus"); break;
-                case '*': System.out.println("into"); break;
-                case '/': System.out.println("by"); break;
-                default: System.out.println("Invalid Operator!!!"); break;
+                System.out.println("BlahBlah");
             }
 
-            System.out.println("Calculate again, Y/N?");
-            action = scan.next().charAt(0);
+            else
+            {   
+                System.out.println("+\t-\t*\t/");
+                action = scan.next().charAt(0);
+
+                switch (action)
+                {
+                    case '+': System.out.println(num1+num2); break;
+                    case '-': System.out.println("minus"); break;
+                    case '*': System.out.println("into"); break;
+                    case '/': System.out.println("by"); break;
+                    default: System.out.println("Invalid Operator!!!"); break;
+                }
+
+                System.out.println("Calculate again, Y/N?");
+                action = scan.next().charAt(0);
+            }
             
         } while ( action != 'N');
     }
