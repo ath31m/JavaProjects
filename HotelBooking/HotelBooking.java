584e4456224a80d75bfb30601528dcd559d5bc25
Min Theim Kyaw <mintheimkyaw18@gmail.com>
2022-06-04 05:02:24 +0630

    created do-while loops on roomTypes and roomStandards

--- HotelBooking/HotelBooking.java
+++ HotelBooking/HotelBooking.java
@@ -23,44 +23,12 @@ public class HotelBooking
         line();
         input.nextLine();
 
-        System.out.println("=== Plz, choose room type ===");
-        System.out.println(" 1: Sea View Suite, $500 ");
-        System.out.println(" 2: Sea View with Pool, $400 ");
-        System.out.println(" 3: Sea View, $300 ");
-        System.out.println(" 4: Garden View, $200 ");
-        System.out.println(" 5: Budget, $100 ");
-        roomType = input.nextInt();
-
-        switch(roomType)
-        {
-            case 1: costType = 500; break;
-            case 2: costType = 400; break;
-            case 3: costType = 300; break;
-            case 4: costType = 200; break;
-            case 5: costType = 100; break;
-            default: System.out.print("Invalid room type");
-        }
+        
 
         input.nextLine();
         line();
 
-        System.out.println("=== Plz, choose room standard ===" + "\n" + "=== Costs will be add-ons ===");
-        System.out.println(" 1: King Size = + $300 ");
-        System.out.println(" 2: Queen Size = + $200 ");
-        System.out.println(" 3: Single-Bedroom = + $100 ");
-        System.out.println(" 4: Double-Bedroom = + $200 ");
-        System.out.println(" 5: Squad-Bedroom = + $300 ");
-        roomStandard = input.nextInt();
-
-        switch(roomStandard)
-        {
-            case 1: costStandard = 300; break;
-            case 2: costStandard = 200; break;
-            case 3: costStandard = 100; break;
-            case 4: costStandard = 200; break;
-            case 5: costStandard = 300; break;
-            default: System.out.print("Invalid room Standard");
-        }
+        
 
         input.nextLine();
         line();
@@ -82,7 +50,53 @@ public class HotelBooking
 
 
 
+public static void type()
+{
+    do
+    {
+        System.out.println("=== Plz, choose room type ===");
+        System.out.println(" 1: Sea View Suite, $500 ");
+        System.out.println(" 2: Sea View with Pool, $400 ");
+        System.out.println(" 3: Sea View, $300 ");
+        System.out.println(" 4: Garden View, $200 ");
+        System.out.println(" 5: Budget, $100 ");
+        roomType = input.nextInt();
 
+        switch(roomType)
+        {
+            case 1: costType = 500; break;
+            case 2: costType = 400; break;
+            case 3: costType = 300; break;
+            case 4: costType = 200; break;
+            case 5: costType = 100; break;
+            default: System.out.println("Invalid room type");
+        }
+    } while(roomType>5);
+}
+
+public static void standard()
+{
+    do
+    {
+        System.out.println("=== Plz, choose room standard ===" + "\n" + "=== Costs will be add-ons ===");
+        System.out.println(" 1: King Size = + $300 ");
+        System.out.println(" 2: Queen Size = + $200 ");
+        System.out.println(" 3: Single-Bedroom = + $100 ");
+        System.out.println(" 4: Double-Bedroom = + $200 ");
+        System.out.println(" 5: Squad-Bedroom = + $300 ");
+        roomStandard = input.nextInt();
+
+        switch(roomStandard)
+        {
+            case 1: costStandard = 300; break;
+            case 2: costStandard = 200; break;
+            case 3: costStandard = 100; break;
+            case 4: costStandard = 200; break;
+            case 5: costStandard = 300; break;
+            default: System.out.println("Invalid room Standard\n");
+        }
+    } while(roomStandard>5);
+}
 
     public static void line()
     {
--- HotelBooking/test.java
+++ HotelBooking/test.java
@@ -22,39 +22,51 @@ public class test
         line();
         input.nextLine();
 
-        System.out.println("=== Plz, choose room type ===");
-        System.out.println(" 1: Sea View Suite, $500 ");
-        System.out.println(" 2: Sea View with Pool, $400 ");
-        System.out.println(" 3: Sea View, $300 ");
-        System.out.println(" 4: Garden View, $200 ");
-        System.out.println(" 5: Budget, $100 ");
-        roomType = input.nextInt();
-
-        switch(roomType)
-        {
-            case 1: costType = 500; break;
-            case 2: costType = 400; break;
-            case 3: costType = 300; break;
-            case 4: costType = 200; break;
-            case 5: costType = 100; break;
-            default: System.out.print("Invalid room type");
-        }
+        type();
         
         input.nextLine();
         line();
 
-        System.out.println("=== Plz, choose number of rooms ===");
-        System.out.println("1: 2: 3: 4: 5: 6: 7: 8: 9: 10 ");
-        numberOfRooms = input.nextInt();
+        number();
 
         calculation();
         
     }
 
+    public static void number()
+    {
+        System.out.println("=== Plz, choose number of rooms ===");
+        System.out.println("1: 2: 3: 4: 5: 6: 7: 8: 9: 10 ");
+        numberOfRooms = input.nextInt();
+    }
+
+    public static void type()
+    {
+       do
+        {
+            System.out.println("=== Plz, choose room type ===");
+            System.out.println(" 1: Sea View Suite, $500 ");
+            System.out.println(" 2: Sea View with Pool, $400 ");
+            System.out.println(" 3: Sea View, $300 ");
+            System.out.println(" 4: Garden View, $200 ");
+            System.out.println(" 5: Budget, $100 ");
+            roomType = input.nextInt();
+
+            switch(roomType)
+            {
+                case 1: costType = 500; break;
+                case 2: costType = 400; break;
+                case 3: costType = 300; break;
+                case 4: costType = 200; break;
+                case 5: costType = 100; break;
+                default: System.out.print("Invalid room type \n"); break;
+            }
+        } while(roomType>5); 
+    }
 
     public static void calculation()
     {
-        System.out.print("The cost is: " + (costType * numberOfRooms));
+        System.out.print("The cost is: " +"\n"+ (costType * numberOfRooms));
     }
 
     public static void line()
