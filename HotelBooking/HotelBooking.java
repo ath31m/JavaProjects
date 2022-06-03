4bdd320a80e39723e35497b722d52a217f4b3fc2
Min Theim Kyaw <mintheimkyaw18@gmail.com>
2022-06-04 04:30:43 +0630

    created roomTypes, costTypes, roomStandard, costStandard

--- HotelBooking/HotelBooking.java
+++ HotelBooking/HotelBooking.java
@@ -4,15 +4,75 @@ public class HotelBooking
 {
     static String customerName;
     static int roomType;
-    static int cost;
+    static int roomStandard;
+    static int numberOfRooms;
+    static int costType;
+    static int costStandard;
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[]args)
     {
-        System.out.println("\n"+"--- Welcome to Hotel California ---"+"\n");   
+        System.out.println("\n"+"=== Welcome to Hotel California ==="+"\n");   
 
         line();
+
+        System.out.print("=== Plz, Enter your name === \n");
+        customerName = input.nextLine();
+
+        line();
+        input.nextLine();
+
+        System.out.println("=== Plz, choose room type ===");
+        System.out.println(" 1: Sea View Suite, $500 ");
+        System.out.println(" 2: Sea View with Pool, $400 ");
+        System.out.println(" 3: Sea View, $300 ");
+        System.out.println(" 4: Garden View, $200 ");
+        System.out.println(" 5: Budget, $100 ");
+        roomType = input.nextInt();
+
+        switch(roomType)
+        {
+            case 1: costType = 500; break;
+            case 2: costType = 400; break;
+            case 3: costType = 300; break;
+            case 4: costType = 200; break;
+            case 5: costType = 100; break;
+            default: System.out.print("Invalid room type");
+        }
+
+        input.nextLine();
+        line();
+
+        System.out.println("=== Plz, choose room standard ===" + "\n" + "=== Costs will be add-ons ===");
+        System.out.println(" 1: King Size = + $300 ");
+        System.out.println(" 2: Queen Size = + $200 ");
+        System.out.println(" 3: Single-Bedroom = + $100 ");
+        System.out.println(" 4: Double-Bedroom = + $200 ");
+        System.out.println(" 5: Squad-Bedroom = + $300 ");
+        roomStandard = input.nextInt();
+
+        switch(roomStandard)
+        {
+            case 1: costStandard = 300; break;
+            case 2: costStandard = 200; break;
+            case 3: costStandard = 100; break;
+            case 4: costStandard = 200; break;
+            case 5: costStandard = 300; break;
+            default: System.out.print("Invalid room Standard");
+        }
+
+        input.nextLine();
+        line();
+
+        System.out.println("=== Plz, choose number of rooms ===");
+        System.out.println("1: 2: 3: 4: 5: 6: 7: 8: 9: 10 ");
+        numberOfRooms = input.nextInt();
+
+
+
+        
     }
+
 
 
 
--- HotelBooking/test.java
+++ HotelBooking/test.java
@@ -1,1 +1,69 @@
+import java.util.Scanner;
 
+public class test
+{
+    static String customerName;
+    static int roomType;
+    static int roomStandard;
+    static int numberOfRooms;
+    static int costType;
+    static int costStandard;
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[]args)
+    {
+        System.out.println("\n"+"=== Welcome to Hotel California ==="+"\n");   
+
+        line();
+
+        System.out.print("=== Plz, Enter your name === \n");
+        customerName = input.nextLine();
+
+        line();
+        input.nextLine();
+
+        System.out.println("=== Plz, choose room type ===");
+        System.out.println(" 1: Sea View Suite, $500 ");
+        System.out.println(" 2: Sea View with Pool, $400 ");
+        System.out.println(" 3: Sea View, $300 ");
+        System.out.println(" 4: Garden View, $200 ");
+        System.out.println(" 5: Budget, $100 ");
+        roomType = input.nextInt();
+
+        switch(roomType)
+        {
+            case 1: costType = 500; break;
+            case 2: costType = 400; break;
+            case 3: costType = 300; break;
+            case 4: costType = 200; break;
+            case 5: costType = 100; break;
+            default: System.out.print("Invalid room type");
+        }
+        
+        input.nextLine();
+        line();
+
+        System.out.println("=== Plz, choose number of rooms ===");
+        System.out.println("1: 2: 3: 4: 5: 6: 7: 8: 9: 10 ");
+        numberOfRooms = input.nextInt();
+
+        calculation();
+        
+    }
+
+
+    public static void calculation()
+    {
+        System.out.print("The cost is: " + (costType * numberOfRooms));
+    }
+
+    public static void line()
+    {
+        for (int i=0; i<60; i++)
+        {
+            System.out.print("-");
+        }
+        System.out.print("\n");
+    }
+
+}
